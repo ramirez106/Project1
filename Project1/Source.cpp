67547cdc685644d939ef65cfc79f7a79651cdacf
ramirez106 <112105048+ramirez106@users.noreply.github.com>
2022-09-14 11:04:16 -0500

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,149 @@
+/*
+	Carlos Ramirez
+	C++ Fall 2022
+	Due: September 14
+	Lab 2 Exploring Outout
+	This lab is designed to get you familiar output to the command prompt 
+	using C++. Your output from your program should follow the format below for full 
+	credit.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string m = "Monday ";
+const string e = "8:00 ";
+const string ca = " Calculus 1 210 ";
+const string d = "10:00 ";
+const string pp = "C++ 162 ";
+const string el = "11:45 ";
+const string ad = "American Diversity 181 ";
+const string t = "Tuesday";
+const string ps = "Public Speaking 112 ";
+const string w = "Wednesday ";
+const string th = "Thursday ";
+const string f = "Friday ";
+const string s = "   ";
+const string os = " ";
+const string st = "  *   *   *   *";
+const string ost = "*   *   *   *";
+
+
+int main()
+{
+	cout << m;
+	cout << s;
+	cout << e;
+	cout << ca;
+
+	cout << endl;
+
+	cout << m;
+	cout << s;
+	cout << d;
+	cout << pp;
+
+	cout << endl;
+
+	cout << m;
+	cout << s;
+	cout << el;
+	cout << ad;
+
+	cout << endl;
+
+	cout << t;
+	cout << s;
+	cout << e;
+	cout << ca;
+
+	cout << endl;
+
+	cout << t;
+	cout << s;
+	cout << d;
+	cout << pp;
+
+	cout << endl;
+
+	cout << t;
+	cout << s;
+	cout << el;
+	cout << ps;
+
+	cout << endl;
+
+	cout << w;
+	cout << e;
+	cout << ca;
+
+	cout << endl;
+
+	cout << w;
+	cout << d;
+	cout << pp;
+
+	cout << endl;
+
+	cout << w;
+	cout << el;
+	cout << ps;
+	
+	cout << endl;
+
+	cout << th;
+	cout << os;
+	cout << e;
+	cout << ca;
+
+	cout << endl;
+
+	cout << th;
+	cout << os;
+	cout << d;
+	cout << pp;
+
+	cout << endl;
+
+	cout << th;
+	cout << os;
+	cout << el;
+	cout << ps;
+
+	cout << endl;
+
+	cout << f;
+	cout << s;
+	cout << d;
+	cout << pp;
+
+	cout << endl;
+
+	cout << f;
+	cout << s;
+	cout << el;
+	cout << ad;
+
+	cout << endl;
+
+	cout << st;
+	cout << endl;
+	cout << ost;
+	cout << endl;
+	cout << st;
+	cout << endl;
+	cout << ost;
+	cout << endl;
+	cout << st;
+	cout << endl;
+	cout << ost;
+	cout << endl;
+	cout << st;
+	cout << endl;
+	cout << ost;
+	cout << endl;
+
+		return 0;
+}
